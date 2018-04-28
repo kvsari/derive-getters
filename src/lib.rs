b4f39482bbce7b69bc4bbbe9ae2beb44c45a44c1
Stephan Luther <kvsari@gmail.com>
2018-04-28 22:53:25 +0900

    WIP: Do some docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,39 @@
-//! # Derive Getters
+//! Derive Getters
+//! ===
+//!
+//! Macro for autogenerating getters. Can only be used on named structs. Will generate
+//! getters that will reside in a new trait so as not to pollute the struct namespace.
+//!
+//! # Usage
+//! Add to your project Cargo.toml;
+//! ```toml
+//! [dependencies]
+//! derive-getters = "0.0.4"
+//! ```
+//!
+//! In lib.rs or main.rs;
+//! ```
+//! #[macro_use]
+//! extern crate derive_getters;
+//! #
+//! # fn main() { }
+//! ```
+//!
+//! # Example
+//! ```
+//! #[macro_use]
+//! extern crate derive_getters;
+//!
+//! #[derive(Getters)]
+//! struct Number {
+//!    num: u64,    
+//! }
+//! 
+//! fn main() {
+//!    let number = Number { num: 655 };
+//!    assert!(number.get_num() == &655);
+//! }
+//! ```
 
 #[macro_use] extern crate quote;
 extern crate proc_macro;
@@ -19,7 +54,22 @@ type TraitSlots = Vec<TraitSlot>;
 type TraitName = Ident;
 type StructName = Ident;
 
-/// For building a list of methods that need to be in the trait.
+/// Derive getters into a seperate trait for the named struct.
+#[proc_macro_derive(Getters)]
+pub fn getters(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    let (trait_tokens, struct_name, trait_name, trait_slots) = setup_getters_trait(&ast);
+    let impl_tokens = setup_trait_impl(struct_name, trait_name, trait_slots);
+
+    let mut tokens = quote::Tokens::new();
+    tokens.append_all(trait_tokens.into_iter());
+    tokens.append_all(impl_tokens.into_iter());
+    
+    //println!("Tokens: {}", &tokens);
+    tokens.into()
+}
+
+// For building a list of methods that need to be in the trait.
 struct TraitSlot {
     label: Ident,
     name: Ident,
@@ -42,22 +92,8 @@ fn field_to_trait_slot(field: &Field) -> TraitSlot {
     TraitSlot::new(label, name, field.ty.clone())
 }
 
-#[proc_macro_derive(Getters)]
-pub fn getters(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-    let (trait_tokens, struct_name, trait_name, trait_slots) = setup_getters_trait(&ast);
-    let impl_tokens = setup_trait_impl(struct_name, trait_name, trait_slots);
-
-    let mut tokens = quote::Tokens::new();
-    tokens.append_all(trait_tokens.into_iter());
-    tokens.append_all(impl_tokens.into_iter());
-    
-    //println!("Tokens: {}", &tokens);
-    tokens.into()
-}
-
-/// Fetch the slots (aka fields) in a structure. If the passed in ast is not a `struct`, it
-/// return `None`.
+// Fetch the slots (aka fields) in a structure. If the passed in ast is not a `struct`, it
+// return `None`.
 fn get_slots<'a>(
     data: &'a syn::Data
 ) -> Result<&'a Punctuated<Field, Comma>, &'static str> {
